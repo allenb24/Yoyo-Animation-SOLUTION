a48a92512cd6dbc2015367db8b8a31db6282dbbc
allenb24 <56567454+allenb24@users.noreply.github.com>
2020-06-15 16:26:56 -0700

    Add project files.

---
+++ Yoyo Animation/Yoyo Animation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	HANDLE screen = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD pos = { 40, 3 };
+	SetConsoleCursorPosition(screen, pos);
+	cout << "O" << endl;
+	Sleep(500);
+
+	for (int tossIt = 1; tossIt <= 8; tossIt++)
+	{
+		// Yoyo unwinds
+
+		while (pos.Y <= 20)
+		{
+			SetConsoleCursorPosition(screen, pos);
+			cout << "|" << endl;
+			pos.Y++;
+			SetConsoleCursorPosition(screen, pos);
+			cout << "O" << endl;
+			Sleep(100);
+		}
+
+		while (pos.Y > 3)
+		{
+			SetConsoleCursorPosition(screen, pos);
+			cout << " " << endl;
+			pos.Y--;
+			SetConsoleCursorPosition(screen, pos);
+			cout << "O" << endl;
+			Sleep(100);
+		}
+	}
+	return 0;
+}
